1aa9612f82487110da441a5ab05031a50309f0aa
olegrewko <olegrewko@gmail.com>
2022-12-25 12:57:44 +0300

    first commit

---
+++ docs/js/main.js
@@ -0,0 +1,14 @@
+$(function(){
+
+	// $('.top__slider').slick({
+	// 	arrows: false,
+	// 	dots: true,
+	// 	autoplay: true,
+	// 	autoplaySpeed: 3000,
+	// 	fade: true
+	// });
+
+	var mixer = mixitup('.portfolio__content');
+
+
+});
