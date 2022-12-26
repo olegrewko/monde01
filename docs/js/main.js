1f52840a5053a255dd3fc3c30b0937b21058988c
olegrewko <olegrewko@gmail.com>
2022-12-26 18:25:10 +0300

    tre

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,23 +1,30 @@
 $(function () {
 
-	// $('.slider-blog__inner').slick({
-	// 	arrows: false,
-	// 	dots: true,
-	// 	autoplay: true,
-	// 	autoplaySpeed: 3000,
-	// 	fade: true
-	// });
+  // $('.slider-blog__inner').slick({
+  // 	arrows: false,
+  // 	dots: true,
+  // 	autoplay: true,
+  // 	autoplaySpeed: 3000,
+  // 	fade: true
+  // });
 
-	var mixer = mixitup('.portfolio__content');
 
-	$('.slider-blog__inner').slick({
-		arrows: true,
-		prevArrow: '<button type="button" class="slick-prev"><img src="img/arrow-left.svg" alt="img/arrow-left.svg" /></button>',
-		nextArrow: '<button type="button" class="slick-next"><img src="img/arrow-right.svg" alt="img/arrow-right.svg" /></button>',
-		dots: true,
-		autoplay: true,
-		autoplaySpeed: 3000,
-		fade: true
-	});
 
+  $('.slider-blog__inner').slick({
+    arrows: true,
+    prevArrow: '<button type="button" class="slick-prev"><img src="img/arrow-left.svg" alt="img/arrow-left.svg" /></button>',
+    nextArrow: '<button type="button" class="slick-next"><img src="img/arrow-right.svg" alt="img/arrow-right.svg" /></button>',
+    dots: true,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    fade: true
+  });
+
+
+  $('.menu__btn').on('click', function name() {
+    $('.menu__list').toggleClass('menu__list--active');
+
+  });
+
+  var mixer = mixitup('.portfolio__content');
 });
